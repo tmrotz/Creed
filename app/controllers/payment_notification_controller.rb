4544846a8b0232d12b8e6c0cc4e9e07a56ab2a17
Travis <tmrotz@gmail.com>
2010-12-02 16:54:30 -0600

    blah blah blah

--- app/controllers/payment_notification_controller.rb
+++ app/controllers/payment_notification_controller.rb
@@ -4,17 +4,17 @@ class PaymentNotificationController < ApplicationController
 
   def create
 
-    if params[:payment_status] == "Completed"
-
-      user_id = params[:item_number].to_i
-      if User.find(user_id).paid == true
-        # WHY WOULD YOU DO THIS AGAIN?!?!?!
-      else
-        User.find(user_id).votes += 1
-        User.find(user_id).paid = true
-      end
-
-    end
+#    if params[:payment_status] == "Completed"
+#
+#      user_id = params[:item_number].to_i
+#      if User.find(user_id).paid == true
+#        # WHY WOULD YOU DO THIS AGAIN?!?!?!
+#      else
+#        User.find(user_id).votes += 1
+#        User.find(user_id).paid = true
+#      end
+#
+#    end
     
     PaymentNotification.create!(:status => params[:payment_status], :txn_id => params[:txn_id], :payer_email => params[:payer_email], :memo => params[:memo], :payment_date => params[:payment_date], :payment_type => params[:payment_type], :quantity => params[:quantity])
     render :nothing => true
