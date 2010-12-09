7fa377d5bad202ba94ae06f30e54646ffeb4f283
Travis <tmrotz@gmail.com>
2010-12-08 19:33:35 -0600

    Added logic to update user accounts with IPN

--- app/controllers/payment_notification_controller.rb
+++ app/controllers/payment_notification_controller.rb
@@ -5,6 +5,11 @@ class PaymentNotificationController < Application
   def create
 
     if (params[:secret] == "amc95n4ioe9gjg")
+      user_id = params[:item_number].to_i
+      user = User.find(user_id)
+      user.votes += 1
+      user.paid = true
+      
       PaymentNotification.create!(:status => params[:payment_status], :txn_id => params[:txn_id], :payer_email => params[:payer_email], :memo => params[:memo], :payment_date => params[:payment_date], :payment_type => params[:payment_type], :quantity => params[:quantity])
     end
     
