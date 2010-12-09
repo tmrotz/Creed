663100b33d0694ba80331b7d1f483bb7701be8d2
Travis <tmrotz@gmail.com>
2010-12-08 19:18:56 -0600

    Finally got IPN to send secret; This will finish the secret token crap

--- app/controllers/payment_notification_controller.rb
+++ app/controllers/payment_notification_controller.rb
@@ -3,8 +3,11 @@ class PaymentNotificationController < Application
   protect_from_forgery :except => [:create]
 
   def create
+
+    if (params[:secret] == "amc95n4ioe9gjg")
+      PaymentNotification.create!(:status => params[:payment_status], :txn_id => params[:txn_id], :payer_email => params[:payer_email], :memo => params[:memo], :payment_date => params[:payment_date], :payment_type => params[:payment_type], :quantity => params[:quantity])
+    end
     
-    PaymentNotification.create!(:status => params[:payment_status], :txn_id => params[:txn_id], :payer_email => params[:payer_email], :memo => params[:memo], :payment_date => params[:payment_date], :payment_type => params[:payment_type], :quantity => params[:quantity])
     render :nothing => true
     
   end
