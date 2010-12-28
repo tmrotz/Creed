e3906377e1f33866f30d84044cb4b435712035cb
Travis <tmrotz@gmail.com>
2010-12-27 23:11:53 -0600

    railsplayground mail fix

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,8 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address => "localhost",
-  :port => 26,
+  :address => "trconsulting.railsplayground.net",
+  :port => 25,
+  :domain => "trconsulting.railsplayground.net",
   :user_name => "trconsul",
   :password => "1amAmulek",
   :authentication => :login
