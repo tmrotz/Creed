b66cc54e8a1c3b996035ad873bac9547c95da71e
Travis <tmrotz@gmail.com>
2011-01-08 22:34:56 -0600

    checking to see if using remote_cache updates using Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
 
-set :deploy_to, "/home/trconsul"
+set :deploy_to, "/home/trconsul/Creed"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 set :scm_verbose, true
@@ -21,6 +21,8 @@ default_run_options[:pty] = true
 
 namespace :deploy do
   task :restart do
-    run "touch /home/trconsul/Creed/tmp/restart.txt"
+    run "touch /home/trconsul/Creed/current/tmp/restart.txt"
+    run "ln -s ~/Creed/current/public ~/public_html"
+    run "chmod 755 ~/Creed/current/public -R"
   end
 end
