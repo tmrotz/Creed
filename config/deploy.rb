a37cb41a6d8c9fe987ba114e0c013de771b0c4d2
Travis <tmrotz@gmail.com>
2011-01-08 19:00:03 -0600

    Getting ready to initiate Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,26 @@
+
 set :application, "Creed"
 set :repository,  "git@github.com:tmrotz/Creed.git"
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, "git"
+set :branch, "master"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, "trconsul"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :domain, "trconsulting.railsplayground.net"
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+set :deploy_to, "/home/trconsul/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :scm_verbose, true
+set :default_run_options[:pty] = true
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/Creed/tmp/restart.txt}"
+  end
+end
