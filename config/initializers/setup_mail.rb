23ca5163179b5f20874df257fb63a1750cf8a019
Travis <tmrotz@gmail.com>
2011-01-10 15:04:39 -0600

    Trying to fix Mailer issues

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,23 +1,25 @@
 
 ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
 
-if Rails.env.development?
-  ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => "tmrotz",
-    :password => "1amAmulek",
-    :authentication => "plain",
-    :enable_starttls_auto => true
-  }
-elsif Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
-    :address => "localhost",
-    :port => 25,
-    :domain => "creed-bplan.org",
-    :user_name => "creed-bplan.org@creed-bplan.org",
-    :password => "1amAmulek",
-    :authentication => :login,
-    :enable_starttls_auto => false
-  }
-end
+#if Rails.env.development?
+#  ActionMailer::Base.smtp_settings = {
+#    :address => "smtp.gmail.com",
+#    :port => 587,
+#    :user_name => "tmrotz",
+#    :password => "1amAmulek",
+#    :authentication => "plain",
+#    :enable_starttls_auto => true
+#  }
+#elsif Rails.env.production?
+#  ActionMailer::Base.smtp_settings = {
+#    :address => "localhost",
+#    :port => 25,
+#    :domain => "creed-bplan.org",
+#    :user_name => "creed-bplan.org@creed-bplan.org",
+#    :password => "1amAmulek",
+#    :authentication => :login,
+#    :enable_starttls_auto => false
+#  }
+#end
